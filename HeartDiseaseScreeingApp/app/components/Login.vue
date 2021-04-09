4ff66ee2b02eabfc24439224542a114386dc213e
Isaac Nathoo <37516124+isaacnathoo@users.noreply.github.com>
2021-04-08 21:24:42 -0400

    pushing login and registration pages

---
+++ HeartDiseaseScreeingApp/app/components/Login.vue
@@ -0,0 +1,199 @@
+<template>
+    <Page @loaded="pageLoaded" class="page">
+        <ActionBar title="Login">
+            <NavigationButton visibility="collapsed" />
+        </ActionBar>
+        <StackLayout class="form">
+            <Image class="logo" src="~/images/logo2.png" />
+            <Label class="header" text="Heart Disease Screening App"></Label>
+            <StackLayout class="input-field">
+                <TextField ref="email" class="input" hint="Email"
+                    keyboardType="email" autocorrect="false"
+                    autocapitalizationType="none" v-model="user.email"
+                    returnKeyType="next" @returnPress="focusPassword()">
+                </TextField>
+                <StackLayout class="hr-light"></StackLayout>
+            </StackLayout>
+            <StackLayout class="input-field">
+                <TextField ref="password" id="password" class="input"
+                    hint="Password" secure="true" v-model="user.password"
+                    returnKeyType="done"></TextField>
+                <StackLayout class="hr-light"></StackLayout>
+            </StackLayout>
+            <Button text="Login" @tap="submit()"
+                class="btn btn-primary m-t-20" style="color: white"></Button>
+            <Label text="Forgot your password?" class="login-label"
+                @tap="forgotPassword()"></Label>
+            <Label @tap="toggleForm()" v-if="isLoggingIn"
+                class="sign-up-group">
+                <FormattedString>
+                    <Span text="Don’t have an account? "
+                        class="sign-up-label">
+                    </Span>
+                    <Span text="Sign up" class="sign-up-label-bold">
+                    </Span>
+                </FormattedString>
+            </Label>
+            <Label @tap="toggleForm()" v-if="!isLoggingIn"
+                class="sign-up-group">
+                <FormattedString>
+                    <Span text="Back to Login" class="sign-up-label-bold">
+                    </Span>
+                    <Span text="" class="sign-up-label-bold">
+                    </Span>
+                </FormattedString>
+            </Label>
+        </StackLayout>
+
+
+    </Page>
+</template>
+<script>
+    import HelloWorld from "./HelloWorld";
+    import UserRegistration from "./UserRegistration";
+    import {
+        isIOS
+    } from "tns-core-modules/platform";
+    import {
+        topmost
+    } from "ui/frame";
+    export default {
+        name: "Login",
+        data() {
+            return {
+                user: {
+                    email: null,
+                    password: null
+                },
+                isLoggingIn: true
+            };
+        },
+        created() {},
+        methods: {
+            pageLoaded() {
+                //PAGE LOADED
+            },
+            submit() {
+                if (!this.user.email || !this.user.password) {
+                    this.alert(
+                        "Please provide both an email address and password."
+                    );
+                    return;
+                }
+                this.login();
+            },
+            login() {
+                // ADD YOUR OWN CODE
+                this.$navigateTo(HelloWorld, {
+                    props: {
+                        username: this.user.email
+                    }
+                });
+            },
+            forgotPassword() {
+                prompt({
+                    title: "Forgot Password",
+                    message: "Enter the email address you used to register for this app to reset your password.",
+                    inputType: "email",
+                    defaultText: "",
+                    okButtonText: "Ok",
+                    cancelButtonText: "Cancel"
+                }).then(data => {
+                    if (data.result) {
+                        // ADD YOUR OWN CODE
+                    }
+                });
+            },
+            focusPassword() {
+                this.$refs.password.nativeView.focus();
+            },
+            alert(message) {
+                return alert({
+                    title: "Heart Disease Screening App",
+                    okButtonText: "OK",
+                    message: message
+                });
+            },
+            toggleForm() {
+                this.isLoggingIn = !this.isLoggingIn;
+                this.$navigateTo(UserRegistration);
+            }
+        }
+    };
+</script>
+
+<style>
+    .page {
+        align-items: center;
+        flex-direction: column;
+    }
+
+    .form {
+        margin-left: 30;
+        margin-right: 30;
+        flex-grow: 2;
+        vertical-align: middle;
+    }
+
+    .logo {
+        margin-bottom: 12;
+        height: 120;
+        font-weight: bold;
+    }
+
+    .header {
+        horizontal-align: center;
+        font-size: 25;
+        font-weight: 600;
+        margin-bottom: 70;
+        text-align: center;
+        color: #D51A1A;
+    }
+
+    .input-field {
+        margin-bottom: 25;
+    }
+
+    .input {
+        font-size: 18;
+        placeholder-color: #A8A8A8;
+    }
+
+    .btn-primary {
+        height: 50;
+        margin: 30 5 15 5;
+        background-color: #D51A1A;
+        border-radius: 5;
+        font-size: 20;
+        font-weight: 600;
+    }
+
+    .login-label {
+        margin-top: 20;
+        horizontal-align: center;
+        color: #A8A8A8;
+        font-size: 16;
+    }
+
+    .sign-up-group {
+        margin-top: 50;
+        horizontal-align: center;
+        color: #A8A8A8;
+        font-size: 16;
+    }
+
+    .sign-up-label {
+        color: #A8A8A8;
+        font-size: 16;
+    }
+
+    .sign-up-label-bold {
+        font-weight: bold;
+        color: #808080;
+        font-size: 16;
+    }
+
+    .bold {
+        color: #000000;
+    }
+</style>
